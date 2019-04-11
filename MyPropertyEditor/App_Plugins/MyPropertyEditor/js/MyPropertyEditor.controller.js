b710f4d1cd88022c01b8f0ce48762e7b44a33200
Paul Seal <prjseal@gmail.com>
2019-04-11 13:30:09 +0100

    - got it all working

--- MyPropertyEditor/App_Plugins/MyPropertyEditor/js/MyPropertyEditor.controller.js
+++ MyPropertyEditor/App_Plugins/MyPropertyEditor/js/MyPropertyEditor.controller.js
@@ -1,4 +1,25 @@
-﻿function giphyPropertyEditorController($scope, $http) {
+﻿function myPropertyEditorController($scope, editorService) {
+
+    $scope.add = function () {
+        var giphyPicker = {
+            apikey: $scope.model.config.apikey,
+            title: "Animated Gif Search",
+            view: "/App_Plugins/MyPropertyEditor/MyPicker.html",
+            size: "small",
+            submit: function (model) {
+                $scope.model.value = model.images.original.url;
+                editorService.close();
+            },
+            close: function () {
+                editorService.close();
+            }
+        };
+        editorService.open(giphyPicker);
+    };
+
+    $scope.remove = function () {
+        $scope.model.value = null;
+    };
 
 }
 angular.module('umbraco').controller("myPropertyEditorController", myPropertyEditorController);
